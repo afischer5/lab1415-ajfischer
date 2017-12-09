3a47f7819e3f74a4c9b822b5b562665a78e03a30
Andrew Fischer <afischer5@luc.edu>
2017-12-09 23:09:37 +0000

    added first few steps

---
+++ src/main/java/edu/luc/cs271/graph/codes.java
@@ -0,0 +1,14 @@
+public class codes{
+  
+  final static String FR = "France";
+  final static String AD = "Andora";
+  final static String ES = "Spain";
+  final static String BE = "Belgium";
+  final static String LU = "Luxemburg";
+  final static String CH = "Switzerland";
+  final static String IT = "Italy";
+  
+  
+  
+  
+}
---
+++ src/main/java/edu/luc/cs271/graph/main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.*;
+
+public class main{
+public static void main(String[] args){
+  
+  final Graph<String, DefaultEdge> myMap = new SimpleGraph<>(DefaultEdge.class);
+  
+    myMap.addVertex(codes.FR);
+    myMap.addVertex(codes.AD);
+    myMap.addVertex(codes.ES);
+    myMap.addVertex(codes.BE);
+    myMap.addVertex(codes.LU);
+    myMap.addVertex(codes.CH);
+    myMap.addVertex(codes.IT);
+  
+}
+}
